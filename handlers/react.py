e24fa608dcbdf80e0ec57456dfc77b870bf5394c
ZeoXD <eren.jeager1929101@gmail.com>
2025-05-01 19:11:36 +0500

    updated react.py

--- handlers/react.py
+++ handlers/react.py
@@ -10,14 +10,11 @@ async def react_handler(ctx, msg, chat_histories_google_sdk):
         await ctx.reply("Hold up!🤨 What are you reacting to? Maybe you forgot to ask a question first dumbass🙄")
         return
 
-    final_prompt = f"""
-                {react_prompt}\n
-                Discord member id: {ctx.author.id}
-                """
+    final_prompt = f"{react_prompt}\nDiscord member id: {ctx.author.id}"
 
     try:
-        response = langchain_api(prompt=final_prompt, request=msg, file_path="react.log",
-                                 chat_history=chat_histories_google_sdk)
+        response = await langchain_api(prompt=final_prompt, request=msg, file_path="react.log",
+                                       chat_history=chat_histories_google_sdk)
 
     except Exception as e:
         response = "An error occurred while processing your request. Please try again later."
